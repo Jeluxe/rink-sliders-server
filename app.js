d0aaacf54c35d93961ccadff45aca2ec4141fa92
shay <metalshay54@gmail.com>
2022-06-14 13:58:04 +0300

    changes

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const cors = require('cors');
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-export const io = new Server(server);
+const io = new Server(server);
+module.exports = io;
 require('./socket');
 
 
